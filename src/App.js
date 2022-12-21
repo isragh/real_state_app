1867dd41eafc7624b6850a54e355f5d68c8570f1
isra <isragonher@gmail.com>
2022-12-21 17:01:26 +0100

    firebase authentication

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
+
 import Explore from './components/routes/Explore';
 import Offers from './components/routes/Offers';
 import Profile from './components/routes/Profile';
@@ -15,13 +18,15 @@ function App() {
         <Routes>
           <Route path='/' element={<Explore />} />
           <Route path='/offers' element={<Offers />} />
-          <Route path='/profile' element={<SignIn />} />
-          <Route path='/sig-in' element={<SignIn />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/sign-in' element={<SignIn />} />
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
         </Routes>
         <Navbar />
       </Router>
+
+      <ToastContainer />
     </>
   );
 }
--- src/components/routes/Profile.jsx
+++ src/components/routes/Profile.jsx
@@ -1,7 +1,18 @@
+import { useState, useEffect } from 'react';
+import { getAuth } from 'firebase/auth';
+
 function Profile() {
-  return (
-    <div>Profile</div>
-  )
+  const [user, setUser] = useState({})
+
+  const auth = getAuth();
+
+  useEffect(() => {
+    setUser(auth.currentUser);
+  }, []);
+
+  return user
+    ? <h1>Hello {user.displayName}</h1>
+    : 'Please, sign in';
 }
 
-export default Profile
+export default Profile;
